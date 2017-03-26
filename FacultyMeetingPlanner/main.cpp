9798a5b6ac548ea2eccc858b7efbd12cc40d0c6b
jvpagan17 <jvpagan17@gmail.com>
2017-03-26 12:32:53 -0400

    Initial Commit

---
+++ FacultyMeetingPlanner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FacultyMeetingPlanner
+//
+//  Created by Jose Pagan on 3/26/17.
+//  Copyright © 2017 Jose Pagan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
